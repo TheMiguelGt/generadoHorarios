8ec671c13cd1ee4f364a7943f0c2510c60659508
TheMiguelGt <miguel.74ngel@gmail.com>
2022-08-30 08:21:02 -0500

    Configuracion de views y urls para poder mostrar el html

--- generadoHorarios/core/views.py
+++ generadoHorarios/core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from multiprocessing import context
+from django.views.generic import TemplateView
 
 # Create your views here.
+class HomePageView(TemplateView): #se crea una clase para poder hacer la vista del home
+    template_name = 'core/home.html'
+    #se obtiene la ruta por medio de la variable 
+    def get(self, request, *args, **kwargs):#importante traer los argumentos y clave valor
+        return render(request,self.template_name,{'title':'Home del generador'})
+
+    
