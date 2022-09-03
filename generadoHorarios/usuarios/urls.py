31b5558b7630a98886ab67afa2b5f536f20d3152
TheMiguelGt <miguel.74ngel@gmail.com>
2022-09-03 13:43:46 -0500

    se agregaron cosas en el html

--- generadoHorarios/usuarios/urls.py
+++ generadoHorarios/usuarios/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import LoginPageView
+from .views import LoginPageView,SignupPageView
 
 urlpatterns = [
     #usuario paths 
     path('login/',LoginPageView.as_view(),name="login"),#vista del login
+    path('signup/',SignupPageView.as_view(),name="signup"), #vista del signup
 ]
