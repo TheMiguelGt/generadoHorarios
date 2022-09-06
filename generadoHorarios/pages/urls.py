65f6be18d877f23c0fc37104824e385f8effe23e
TheMiguelGt <miguel.74ngel@gmail.com>
2022-09-06 17:11:11 -0500

    crear horarios mediante el mismo template

--- generadoHorarios/pages/urls.py
+++ generadoHorarios/pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PageListView,PageDetailView
+from .views import PageListView,PageDetailView,PageCreate
 
-urlpatterns = [
+pages_patterns = ([ #se crea una tupla
     path('',PageListView.as_view(), name='pages'), #se usan las clases y se declaran como vistas
     path('<int:pk>/<slug:slug>/',PageDetailView.as_view(), name='page'), #se cambio el id por pk y slug_page por solo slug
-]
+    path('create/',PageCreate.as_view(),name='create')#path de crear
+],'pages')
--- generadoHorarios/pages/views.py
+++ generadoHorarios/pages/views.py
@@ -1,10 +1,20 @@
 from django.views.generic.list import ListView #crear una lista de paginas
 from django.views.generic.detail import DetailView #detalles de la pagina
+from django.views.generic.edit import CreateView #crear nuevas vistas
+from django.urls import reverse, reverse_lazy
 from .models import Page
 
 # Create your views here.
-class PageListView(ListView):
+class PageListView(ListView):#listar
     model = Page    #se obtiene el modelo de la app
 
-class PageDetailView(DetailView):
+class PageDetailView(DetailView):#ver detalles
     model = Page
+    
+class PageCreate(CreateView):#crear
+    model = Page
+    fields = ['title','content','order']
+    success_url = reverse_lazy('pages:pages') #se puede hacer de dos maneras 
+    
+    #def get_success_url(self): #metodo cuando se haya creado un nuevo horario, donde nos mandara al listado
+     #   return reverse('pages:pages')
