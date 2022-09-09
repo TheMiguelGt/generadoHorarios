25dd1ec384138225d7f8585446cade32d6a67086
TheMiguelGt <miguel.74ngel@gmail.com>
2022-09-09 15:55:44 -0500

    identificacion en la parte de materias

--- generadoHorarios/pages/views.py
+++ generadoHorarios/pages/views.py
@@ -1,9 +1,18 @@
 from django.views.generic.list import ListView #crear una lista de paginas
 from django.views.generic.detail import DetailView #detalles de la pagina, solo para lectura
 from django.views.generic.edit import CreateView,UpdateView,DeleteView #crear nuevas vistas,actualizar y eliminar 
+from django.contrib.admin.views.decorators import staff_member_required #personas del staff
+from django.utils.decorators import method_decorator #usar como decorador sus funciones
 from django.urls import reverse, reverse_lazy
+from django.shortcuts import redirect
 from .models import Page
 from .forms import PageForms
+
+class StaffRequiredMixin(object): #clase base de todas las clases de py
+    """El mixin requerira que sea miembro del staff"""
+    @method_decorator(staff_member_required)
+    def dispatch(self,request,*args,**kwargs): #permitir controlar la peticion
+        return super(StaffRequiredMixin,self).dispatch(request,*args,**kwargs)
 
 # Create your views here.
 class PageListView(ListView):#listar
@@ -11,14 +20,16 @@ class PageListView(ListView):#listar
 
 class PageDetailView(DetailView):#ver detalles
     model = Page
-    
-class PageCreate(CreateView):#crear
+
+@method_decorator(staff_member_required,name='dispatch')
+class PageCreate( CreateView):#crear
     model = Page
     form_class = PageForms #se pasa la clase que se creo 
     success_url = reverse_lazy('pages:pages') #se puede hacer de dos maneras 
     #def get_success_url(self): #metodo cuando se haya creado un nuevo horario, donde nos mandara al listado
      #   return reverse('pages:pages')
 
+@method_decorator(staff_member_required,name='dispatch')
 class PageUpdate(UpdateView):
     model = Page
     form_class = PageForms #campos para actualizar
@@ -27,6 +38,7 @@ class PageUpdate(UpdateView):
     def get_success_url(self): #mostrar el formulario para ver los cambios
         return reverse_lazy('pages:update', args=[self.object.id]) + '?ok' #se recrea la url, donde se le pasa el update y la clave primaria con el indicador 
     
+@method_decorator(staff_member_required,name='dispatch')
 class PageDelete(DeleteView):#eliminar
     model = Page 
     success_url = reverse_lazy('pages:pages')
