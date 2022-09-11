2c40a6312062e21a24269ef15219fd7a4effd7e7
TheMiguelGt <miguel.74ngel@gmail.com>
2022-09-11 14:35:12 -0500

    editar perfil y cambios en algunos html

--- generadoHorarios/registration/models.py
+++ generadoHorarios/registration/models.py
@@ -1,3 +1,9 @@
+from distutils.command.upload import upload
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) #identifica un perfil para cada usuario
+    avatar = models.ImageField(upload_to='profiles', null=True, blank=True)
+    
