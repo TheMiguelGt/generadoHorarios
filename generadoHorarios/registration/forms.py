c7ef47225cd6f910ee71c7c80304200a3cbc2ccb
TheMiguelGt <miguel.74ngel@gmail.com>
2022-09-16 14:41:51 -0500

    campos en editar perfil y cambiar la img default por la que uno desee

--- generadoHorarios/registration/forms.py
+++ generadoHorarios/registration/forms.py
@@ -1,6 +1,7 @@
 from django import forms 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 class UserCreationFormWithEmail(UserCreationForm):
     email = forms.EmailField(required=True,help_text="Requerido, 254 caracteres como maximo y debe ser valido")
@@ -14,3 +15,12 @@ class UserCreationFormWithEmail(UserCreationForm):
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError("El email ya está registrado, prueba con otro.")
         return email
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['avatar','bio']
+        widgets = {
+            'avatar': forms.ClearableFileInput,
+            'bio': forms.Textarea(attrs={'placeholder':'Biografia'}),
+        }
