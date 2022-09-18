5c9cfd77624f7f83eec7a48ea71565299948879a
TheMiguelGt <miguel.74ngel@gmail.com>
2022-09-18 14:04:18 -0500

    se cambio la configuracion a sql

--- generadoHorarios/generadoHorarios/settings.py
+++ generadoHorarios/generadoHorarios/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'generadoHorarios.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ghea',
+        'USER': 'root',
+        'PASSWORD': '1ng3n13r14',
+        'PORT': 3306,
+        'HOST': 'localhost',
     }
 }
 
