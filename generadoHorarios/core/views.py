8073889efe36833672898656d222ecb6b87e1dc0
TheMiguelGt <miguel.74ngel@gmail.com>
2022-10-09 20:37:53 -0500

    Se cambio la ruta del home de usuario

--- generadoHorarios/core/views.py
+++ generadoHorarios/core/views.py
@@ -9,4 +9,10 @@ class HomePageView(TemplateView): #se crea una clase para poder hacer la vista d
     def get(self, request, *args, **kwargs):#importante traer los argumentos y clave valor
         return render(request,self.template_name,{'title':'Home'})
 
+class HomeUserView(TemplateView): #se crea una clase para poder hacer la vista del home
+    template_name = 'core/homeuser.html'
+    #se obtiene la ruta por medio de la variable 
+    def get(self, request, *args, **kwargs):#importante traer los argumentos y clave valor
+        return render(request,self.template_name,{'title':'HomeUser'})
+
     
