117f697d56ea4175058ab3850f9511609b86ab0a
TheMiguelGt <miguel.74ngel@gmail.com>
2022-12-01 19:03:45 -0600

    imagen de usuarios del historial

--- generadoHorarios/core/views.py
+++ generadoHorarios/core/views.py
@@ -6,6 +6,7 @@ from django.views.generic import TemplateView
 from pages.models import Page,Disponibilidad,DocenteMateria
 from django.core.paginator import Paginator
 from usuarios.models import Admin,Coordina,Docente
+from institucion.models import Plantel
 
 # Create your views here.
 class HomePageView(TemplateView): #se crea una clase para poder hacer la vista del home
@@ -38,11 +39,14 @@ def homePage2View(request):
     pages = Page.objects.all()
     disponi = Disponibilidad.objects.all()
     matedo = DocenteMateria.objects.all()
+    admin = Admin.objects.all()
+    coordina = Coordina.objects.all()
+    docente = Docente.objects.all()
     
     print(pages.query)
     page_number = request.GET.get('homeUser')
     page_obj = paginator.get_page(page_number)
-    return render(request,'core/home2.html',{'page_obj': page_obj,'pages':pages,'disponi':disponi,'matedo':matedo})
+    return render(request,'core/home2.html',{'page_obj': page_obj,'pages':pages,'disponi':disponi,'matedo':matedo,'admin':admin,'coordina':coordina,'docente':docente})
     
 class UserPageView(TemplateView):
     template_name = 'core/userhome1.html'
@@ -53,6 +57,17 @@ class UserPageView(TemplateView):
     
     def get(self, request, *args, **kwargs):#importante traer los argumentos y clave valor
         return render(request,self.template_name,{'title':'userPage'})
+    
+    def get_context_data(self, **kwargs):
+        context = super(UserPageView, self).get_context_data(**kwargs)
+        context['history_list'] = Plantel.history.all()
+        context['pages'] = Page.objects.all()
+        context['disponi'] = Disponibilidad.objects.all()
+        context['matedo'] = DocenteMateria.objects.all()
+        context['admin'] = Admin.objects.all()
+        context['coordina'] = Coordina.objects.all()
+        context['docente'] = Docente.objects.all()
+        return context
     
 
     
