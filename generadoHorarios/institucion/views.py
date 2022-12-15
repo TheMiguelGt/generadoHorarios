de0132fb02698db28a1e67b0e362769500e80714
TheMiguelGt <miguel.74ngel@gmail.com>
2022-12-14 22:43:29 -0600

    ya se enlista el horario pdf y view

--- generadoHorarios/institucion/views.py
+++ generadoHorarios/institucion/views.py
@@ -490,14 +490,20 @@ class SemestreDelete(DeleteView):
 def TimeTableView(request,id):
     try:
         section = Semestre.objects.get(id=id)
-        docema = DocenteMateria.objects.select_related('materia','docente','clase').filter(clase_id=section.id).order_by('dia','start_time')
+        docema = DocenteMateria.objects.select_related('materia','docente','clase').filter(clase_id=section.id).order_by('start_time')
+        admin = Admin.objects.all()
+        coordina = Coordina.objects.all()
+        docente = Docente.objects.all()
+        pages = Page.objects.all()
+        disponi = Disponibilidad.objects.all()
+        matedo = DocenteMateria.objects.all()
         print(docema.query)
         time = [0] * (section.end_time - section.start_time)
         time_slot = [''] * (section.end_time - section.start_time)
         for x in range(0, len(time)):
             time_slot[x] = str(section.start_time + x) + ':00 - ' + str(section.start_time + x + 1) + ':00'
             time[x] = section.start_time + x
-        context_1 = {'section':section,'docema':docema,'time':time,'time_slot':time_slot}
+        context_1 = {'section':section,'docema':docema,'time':time,'time_slot':time_slot,'admin':admin,'coordina':coordina,'docente':docente,'pages':pages,'disponi':disponi,'matedo':matedo}
         return render(request,'institucion/horarioTable.html',context_1)
     except Semestre.DoesNotExist:
         messages.error(request, 'La actividad no existe')
@@ -510,7 +516,7 @@ def TimeTableView(request,id):
 def horario_render_pdf_view(request,*args, **kwargs):
     pk = kwargs.get('pk')   
     horario = get_object_or_404(Semestre,pk=pk)
-    docema = DocenteMateria.objects.select_related('materia','docente','clase').filter(clase_id=horario.id).order_by('dia','start_time')
+    docema = DocenteMateria.objects.select_related('materia','docente','clase').filter(clase_id=horario.id).order_by('start_time')
     time = [0] * (horario.end_time - horario.start_time)
     time_slot = [''] * (horario.end_time - horario.start_time)
     for x in range(0, len(time)):
